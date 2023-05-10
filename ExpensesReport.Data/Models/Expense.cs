581e0178ebdb168ab1866665f48abd30d4fb8cd3
brianokanga <briannokanga@gmail.com>
2023-05-10 17:03:09 +0300

    Added more data annotations to the expense model

--- ExpensesReport.Data/Models/Expense.cs
+++ ExpensesReport.Data/Models/Expense.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +11,19 @@ namespace ExpensesReport.Data.Models
 {
     public class Expense
     {
+        [Key]
         public int Id { get; set; }
 
         [Required]
         public string Title { get; set; }
 
         [Required]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(10, 2)")]
         public float Amount { get; set; }
 
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         [Required]
         public DateTime ExpenseDate { get; set; }
 
