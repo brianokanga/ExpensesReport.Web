7069919c3773401463830413262dd36dc7215cac
brianokanga <briannokanga@gmail.com>
2023-05-10 13:46:32 +0300

    Added IExpenseRepository

---
+++ ExpensesReport.Data/Repository/IRepository/IExpenseRepository.cs
@@ -0,0 +1,20 @@
+﻿using ExpensesReport.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpensesReport.Data.Repository.IRepository
+{
+    public interface IExpenseRepository
+    {
+        IEnumerable<Expense> GetAllExpenses();
+        IEnumerable<Expense> Search(string searchString);
+        void Add(Expense expense);
+        int Update(Expense expense);
+        Expense GetExpenseById(int id);
+        void Delete(int id);
+
+    }
+}
